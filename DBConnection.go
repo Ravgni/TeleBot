2651b89441b95f259079172750b28f9a1299de82
IgorPundiev <ipundiev@n-ix.com>
2022-05-25 21:37:59 +0300

    Added mongodb connection

---
+++ DBConnection.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectDB() (*mongo.Client, context.Context) {
+	client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return client, ctx
+}
